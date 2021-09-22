aa9a7694e9f0bd6564b81656dbeae8e1b1d99362
Sergii Ikiro <ikiro@ukr.net>
2021-09-22 21:08:51 +0300

    Buttons Bigger

--- Calaculator.py
+++ Calaculator.py
@@ -5,7 +5,7 @@ entry1=Entry(window,width=60,borderwidth=10,bg="light blue")
 entry1.grid(row=0,column=0,columnspan=4,padx=10,pady=10)
 
 def addButton(num):
-    return Button(window,padx=16,bd=8,fg="blue",text=num,width=5, command=lambda:clickbutton (str(num)))
+    return Button(window,padx=20,bd=8,fg="blue",text=num,width=5, command=lambda:clickbutton (str(num)))
 def createButton():
     btton0=addButton(0)
     btton1=addButton(1)
